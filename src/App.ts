4529b1914726d61e45dd87d6d824af460660efe4
Debankle <oliver.debankle@gmail.com>
2020-02-11 17:31:27 +1000

    Structure improvements

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,10 @@
-import { Response, Application, Request } from "express";
+import { Application } from "express";
+import * as bodyParser from 'body-parser';
+import BackendRouter from './/routes/router';
 
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 class SchoolworkBackend {
     public express: Application;
@@ -9,17 +13,18 @@ class SchoolworkBackend {
     constructor() {
         this.express = express();
         this.port = 4200;
+
+        this.express.use(cors());
+        this.express.use(bodyParser.json());
+        this.express.use(bodyParser.urlencoded({
+            extended: false
+        }));
+
         this.initRoutes();
     }
 
     private initRoutes(): void {
-        const router = express.Router();
-        router.get('/', (req: Request, res: Response) => {
-            res.json({
-                message: 'Hello World!'
-            });
-        });
-        this.express.use('/', router);
+        this.express.use('', BackendRouter);
     }
 }
 
