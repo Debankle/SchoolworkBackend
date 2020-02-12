fdf59958fc82005a1e3da48a895a77e5de12ff6d
Debankle <oliver.debankle@gmail.com>
2020-02-12 17:05:07 +1000

    Improved init file

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,3 @@
 import SchoolworkBackend from './App';
 
-SchoolworkBackend.express.listen(SchoolworkBackend.port, () => {
-    console.log(`Server is running on port ${SchoolworkBackend.port}`);
-}).on('error', (err: Error) => {
-    console.log(err);
-});
+SchoolworkBackend.listen();
