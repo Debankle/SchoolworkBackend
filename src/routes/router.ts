9bbb68d1a6b3cbc937561dd2ba2fa43318d8a4e2
Debankle <oliver.debankle@gmail.com>
2020-02-12 17:04:22 +1000

    Basic working routes with mongo

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,10 +1,32 @@
 import * as express from 'express';
-
+import User, { IUser } from '../models/Schema';
 const BackendRouter = express.Router();
 
 BackendRouter.get('/', (req: express.Request, res: express.Response) => {
     res.send("Hello World!");
 });
 
+BackendRouter.post('/register', (req: express.Request, res: express.Response) => {
+    new User({
+        "name": req.body.name,
+        "email": req.body.email,
+        "year": req.body.year,
+        "subjects": req.body.subjects
+    }).save().then(() => {
+        res.sendStatus(200);
+    }).catch(err => {
+        res.send(err).sendStatus(400);
+    });
+});
+
+BackendRouter.post('/delete/name', (req: express.Request, res: express.Response) => {
+    User.deleteOne({
+        "name": req.body.name
+    }).then(() => {
+        res.sendStatus(200);
+    }).catch(err => {
+        res.send(err).sendStatus(400);
+    });
+});
 
 export default BackendRouter;
