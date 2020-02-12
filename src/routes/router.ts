45538ba0e4745ff9e87e79960b3675d55677f955
Debankle <oliver.debankle@gmail.com>
2020-02-12 21:32:13 +1000

    Implemented post add and remove, very shitty

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,5 +1,8 @@
 import * as express from 'express';
-import User, { IUser } from '../models/Schema';
+import User, { IUser } from '../models/User';
+import Post, { IPost } from '../models/Post';
+import { Schema } from 'mongoose';
+
 const BackendRouter = express.Router();
 
 BackendRouter.get('/', (req: express.Request, res: express.Response) => {
@@ -11,7 +14,9 @@ BackendRouter.post('/register', (req: express.Request, res: express.Response) =>
         "name": req.body.name,
         "email": req.body.email,
         "year": req.body.year,
-        "subjects": req.body.subjects
+        "subjects": req.body.subjects,
+        "admin": req.body.admin,
+        "owner": req.body.owner
     }).save().then(() => {
         res.sendStatus(200);
     }).catch(err => {
@@ -27,6 +32,51 @@ BackendRouter.post('/delete/name', (req: express.Request, res: express.Response)
     }).catch(err => {
         res.send(err).sendStatus(400);
     });
+});
+
+BackendRouter.post('/create/post', (req: express.Request, res: express.Response) => {
+    new Post({
+        "title": req.body.title,
+        "text": req.body.text,
+        "userID": req.body.userID,
+        "subject": req.body.subject,
+        "postDate": Date.now(),
+        "lastEdit": Date.now()
+    }).save().then(() => {
+        res.sendStatus(200);
+    }).catch(err => {
+        res.send(err).sendStatus(400);
+    });
+});
+
+
+BackendRouter.post('/delete/post', (req: express.Request, res: express.Response) => {
+
+    // TODO: create seperate delete function so errors can be handled
+    var admin: Boolean = false;
+    var postID: Schema.Types.ObjectId = req.body.postID;
+    var userID: Schema.Types.ObjectId = req.body.userID;
+
+    User.findById(userID, (err: Error, data: IUser) => {
+        admin = data.admin;
+        Post.findById(postID, (err: Error, data: IPost) => {
+            if (data.userID == userID) {
+                admin = true;
+            }
+            if (admin) {
+                Post.deleteOne({
+                    "_id": postID
+                }).then(() => {
+                    res.send({ errorCode: 200, message: "Successfully deleted post" });
+                }).catch(err => {
+                    res.send({ errorCode: 400, message: err});
+                });
+            } else {
+                res.send({ errorCode: 400, message: "Lacking permissions" });
+            }
+        });
+    });
+
 });
 
 export default BackendRouter;
